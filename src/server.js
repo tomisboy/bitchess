08097bc61643515b31d24c9da8df383afaa9d96d
Jonas Scherer <scherer.jonas22@gmail.com>
2022-03-18 12:02:37 +0100

    include static files

--- src/server.js
+++ src/server.js
@@ -3,27 +3,30 @@ const app = express()
 require('dotenv').config();
 const mongoose = require('mongoose')
 
-
-
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
-
 app.set('view-engine' , 'ejs')
+
+//Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
+//Open on Port
 app.listen(3000)
 
+//Routing Targets
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
-
-
 app.get('/login', (req, res) => {
     res.render('login.ejs')
 })
-
 
 app.get('/register', (req, res) => {
     res.render('register.ejs')
