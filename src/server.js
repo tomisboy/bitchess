b4898b215b74da580f98b6ad07773ef1b5dbd947
Alexander Lutz <alexander.lutz.2002@gmail.com>
2022-03-20 18:54:28 +0100

    login/register

--- src/server.js
+++ src/server.js
@@ -1,11 +1,32 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const initializePassport  = require('./passport-config')
+const flash = require('express-flash')
+const session = require('express-session')
+const db = require('./database.js')
+const methodOverride = require('method-override')
+
 require('dotenv').config();
 
 app.set('view-engine' , 'ejs')
 
+initializePassport (passport)
+
 //Static Files
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/scss', express.static(__dirname + 'public/scss'))
 app.use('/js', express.static(__dirname + 'public/js'))
@@ -16,56 +37,78 @@ app.listen(3000)
 
 //Routing Targets
 app.get('/', (req, res) => {
-    res.render('index.ejs')
-})
-
-app.get('/login', (req, res) => {
     res.render('login.ejs')
-})
-
-app.get('/register', (req, res) => {
-    res.render('register.ejs')
 })
 
 app.get('/homepage', (req, res) => {
     res.render('homepage.ejs')
 })
 
-app.get('/create', (req, res) => {
+app.get('/create', checkAuthenticated, (req, res) => {
     res.render('create.ejs')
 })
 
-app.get('/join', (req, res) => {
+app.get('/join', checkAuthenticated, (req, res) => {
     res.render('join.ejs')
 })
 
-app.get('/statistics', (req, res) => {
+app.get('/statistics', checkAuthenticated, (req, res) => {
     res.render('statistics.ejs')
 })
 
-app.get('/createpublic', (req, res) => {
+app.get('/createpublic', checkAuthenticated, (req, res) => {
     res.render('createpublic.ejs')
 })
 
-app.get('/createprivate', (req, res) => {
+app.get('/createprivate', checkAuthenticated, (req, res) => {
     res.render('createprivate.ejs')
 })
 
-app.get('/joinpublic', (req, res) => {
+app.get('/joinpublic', checkAuthenticated, (req, res) => {
     res.render('joinpublic.ejs')
 })
 
-app.get('/joinprivate', (req, res) => {
+app.get('/joinprivate', checkAuthenticated, (req, res) => {
     res.render('joinprivate.ejs')
 })
 
 //Post-Methods
-app.post('/loginsubmit', function (req, res) {
-    console.log(req.body)
-    res.redirect('/bla')
+app.post('/loginsubmit', passport.authenticate('local',{
+    successRedirect: '/homepage',
+    failureRedirect: '/',
+    failureFlash: true
+}))
+
+app.post('/registersubmit', async (req,res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+        db.setUser(
+            req.body.username,
+            hashedPassword,
+            req.body.email
+        );
+        res.redirect('/')
+    } catch (e){    
+        res.redirect('/')
+    }
 })
 
-app.post('/registersubmit', function (req,res) {
-    console.log(req.body)
-    res.redirect('/bla')
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
 })
+
+function checkAuthenticated(req, res, next){
+    if (req.isAuthenticated()){
+        return next()
+    }
+    res.redirect('/')
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated){
+        return res.redirect('/')
+    }
+    next()
+}
