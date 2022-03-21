0af4351f5a15fd9532a6dea4d67a4171c52d014a
Alexander Lutz <alexander.lutz.4.2014@gmail.com>
2022-03-21 17:27:38 +0100

    [Bugfix] db crash bug https://github.com/tomisboy/bitchess/issues/5

--- src/database.js
+++ src/database.js
@@ -2,51 +2,51 @@ var mysql = require('mysql');
 var db = require("./database.js")
 require('dotenv').config();
 
-
-var con = mysql.createConnection({
-     host: process.env.DB_HOST, 
-     user:'root', 
-     password: process.env.DB_PASSWORD,
-     database: 'bitchess',
-     connectionLimit: 5
-});
-
-con.connect((err) => {
-    if (err) {
-      console.log("Error occurred", err);
-    } else {
-      console.log("Connected to MySQL Server");
-    }
-});
+var dbconfig = {
+  host: process.env.DB_HOST, 
+  user:'root', 
+  password: process.env.DB_PASSWORD,
+  database: 'bitchess',
+  connectionLimit: 5
+}
 
 module.exports = {
   getUser: function(email, callback){
     try{
-        con.query("SELECT * FROM users WHERE email = ?", [email], function (err, result) {
-            if (err) throw err; 
-            callback(result[0])        
-        })
+      var con = mysql.createConnection(dbconfig);
+      con.connect()
+      con.query("SELECT * FROM users WHERE email = ?", [email], function (err, result) {
+        if (err) throw err; 
+        callback(result[0])        
+      })
+      con.end()
     } catch {          
             callback(null);      
     }  
   },
   getUserById: function(id, callback){
     try{
+        var con = mysql.createConnection(dbconfig);
+        con.connect()
         con.query("SELECT * FROM users WHERE id = ?", [id], function (err, result) {
             if (err) throw err; 
             callback(result[0])        
         })
+        con.end()
     } catch {          
             callback(null);      
     }  
   },
   setUser: function(username, password, email){  
     try{
-        con.query("INSERT INTO users (username, password, email) VALUES (?,?,?)", [username, password, email], function (err, result){
-            if (err) throw err;
-            }); 
+      var con = mysql.createConnection(dbconfig);
+      con.connect()
+      con.query("INSERT INTO users (username, password, email) VALUES (?,?,?)", [username, password, email], function (err, result){
+          if (err) throw err;
+      })
+      con.end()
     } catch (e) {
         console.log(e)
     }
   }
-} 
+}
