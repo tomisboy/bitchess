55397d7e2e5454400d653c000fd2171ef4254a1f
Alexander Lutz <alexander.lutz.4.2014@gamil.com>
2022-03-26 17:30:24 +0100

    botgame gets saved to db

--- Database/phpmyadmin/db_config.sql
+++ Database/phpmyadmin/db_config.sql
@@ -9,6 +9,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `botgames`
+--
+
+CREATE TABLE `botgames` (
+  `id` int(11) NOT NULL,
+  `playerid` int(11) NOT NULL,
+  `board` text COLLATE utf8_bin NOT NULL,
+  `moves` text COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `games`
 --
 
@@ -51,8 +64,23 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `email`, `password`, `id`, `rating`) VALUES
+('alex@alex.alex', 'alex@alex.alex', '$2b$10$B.Bhk7auEWXbLC4rIi.OGeBeBZvvIlfaksDmKPXSWu8kL1coflJFO', 1, 800),
+('jonas@jonas.jonas', 'jonas@jonas.jonas', '$2b$10$IW5Gemt9a75HhVf0RopzHu9YU1430J9erQAnFnSPc8MsbkBsyaSa.', 7, 800),
+('thomas@thomas.thomas', 'thomas@thomas.thomas', '$2b$10$ElmfbEzAXZpfUaJSrrtDzOIGmjs9fLkBerZB0plmMFTqB7t4PiYXG', 9, 800);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `botgames`
+--
+ALTER TABLE `botgames`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `games`
@@ -77,6 +105,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `botgames`
+--
+ALTER TABLE `botgames`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `games`
 --
 ALTER TABLE `games`
@@ -92,5 +126,5 @@ ALTER TABLE `library`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
