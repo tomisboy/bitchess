811ec81db69843e84e77b16b9830bf63e8a773bf
Jonas Scherer <scherer.jonas22@gmail.com>
2022-03-29 14:57:58 +0200

    pack data into leaderboard scrollcontainer

--- src/public/js/leaderboard.js
+++ src/public/js/leaderboard.js
@@ -6,4 +6,8 @@ $(document).ready(function (){
 
 function setLeaderboard(data){
     console.log(data)
+    var leaderboardList = document.getElementById('leaderboardList')
+    for(var i=1; i <= data.length; i++) {
+        leaderboardList.innerHTML += '<li>' + i + data[i-1].username + data[i-1].rating + '</li>'
+    }
 }
