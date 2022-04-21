11a20f7bef11a2e2fd1487f67e34b66bca254093
Alexander Lutz <alexander.lutz.4.2014@gmail.com>
2022-04-21 13:28:20 +0200

    change port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const app = require('./app/app')
 const http = require('http')
 const socket = require('socket.io')
 
-const PORT = process.env.PORT || 443
+const PORT = 3000
 
 var server = http.createServer(app).listen(PORT, function(){
     console.log("Express server listening on port " + PORT);
