ba1463185a883637e902865203bdbd3f397530ae
Thomas Alpert <thomasalpert@gmail.com>
2022-04-29 19:44:59 +0200

    Multiplayer Chess working added Comments

--- src/app/socketServer.js
+++ src/app/socketServer.js
@@ -2,95 +2,76 @@ const db = require('./database.js')
 
 
 
-const users = [];
-function randomRoomId() {
+const users = []; // Serverseitiges Array, welches aktuelle online Spieler speichert
+function randomRoomId() { // funktion um einen zufälligen raumnamen zu erstellen
     let roomId = '';
     let length = 4;
     let randomChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
     for (counter = 0; counter < length; counter++) {
         roomId += randomChar.charAt(Math.floor(Math.random() * randomChar.length));
     }
-
     return roomId;
-
 }
 
 
 exports = module.exports = function (io) {
 
     io.on('connection', (socket) => {
+        //   Sobald eine Socket Clientseitig initialisiert wird, wird dieses Code ausgeführt:
 
-        socket.on('StartLobby', (requestData) => {
-            console.log(requestData)
 
+        //     ########## LOBBY ###############
+        //          Lobby Funktionen
+
+        socket.emit('lobbyrefresh', { //Schicke das Event "load_waiting_players" ans Frontend um die Lobby Liste zu laden
+            users: users, //gebe die Liste der aktuell online und spielbereiten Spieler mit                   
+        });
+
+
+
+        socket.on('StartLobby', (requestData) => {  // Funktion um Client spielbereit zu machen (in die Lobby aufzunehmen)
+            // Übergabeparameter beinhaltet aktuellen Usernamen und UserID
             let room = randomRoomId();
             let username = requestData.name;
-            let userid = requestData.userid
 
 
-            
-            users.push({
+            users.push({  // User in das globale Serverseite UserArray aufnehmen
                 id: socket.id,
                 name: username,
                 room: room,
-                userid: userid
             });
-            socket.join(room);
+            socket.join(room); //User Joint den Socker Raum
             console.log(users);
 
-            socket.broadcast.emit("roomDetail", { // Melde allen dass du zu spielen bereit bist
-                users: users
-
-
+            socket.broadcast.emit("lobbyrefresh", { // Trigger das Refreshen der globalen Lobby 
+                //socket.broadcast.emit sendet dieses Event an ALLE Teilnehmer die mit dem Socket/Server verbunden sind
+                users: users //gebe die globale aktuelle online Teilnehmerliste mit 
             });
 
         });
 
-        socket.emit('existingUsers', {
-            users: users,
-            currentUserId: socket.id
-        });
-
-        //        socket.on('triggerStart', (requestData) => {
-        //            let user = users.filter(user => user.id == socket.id)[0];
-        //            socket.broadcast.to(requestData.room).emit('startgame', {
-        //                id: user.id,
-        //                name: user.name,
-        //                room: user.room
-        //            })
-        //        });
 
 
         socket.on('sendJoinRequest', (requestData) => {
-            //console.log(requestData.room);
-            let user = users.filter(user => user.id == socket.id)[0];
-            console.log("list rooms and participants")
+            socket.join(requestData.room) //Der Anfragende verbindet sich nun in den Socket/Raum
+            console.log(users);
 
-
-            console.log(users)
-            console.log(user.name)
-            console.log("from room" + user.room)
-            console.log("send JoinRequest... to room ")
-            console.log(requestData.room)
-            socket.join(requestData.room)
-            user.room = requestData.room
-            console.log("trete raum bei")
-            console.log("Zeige aktuellen raum")
-            console.log(users)
-
-
+            let user = users.filter(user => user.id == socket.id)[0]; //hole die User der die Anfrag gesendet hat aus der List aller User
+            user.room = requestData.room  // setze den room des users auf den, den er beigetreten ist
             socket.broadcast.to(requestData.room).emit('joinRequestRecieved', {
-                id: user.id,
+                //sendet die Info/EVENT, dass sich jemand mit der Lobby verbinden will als Broadcast in dem Raum
+                //dieses EVENT landet zwagsläufig beim Herausforderer/Host
+                // Anschließend werden noch infos mitgegeben wer die Anfrage gesendet hat:
+                id: user.id,  
                 name: user.name,
                 room: user.room
             });
         });
 
-        socket.on('acceptGameRequest', (requestData) => {
+        socket.on('acceptGameRequest', () => { // middleware funktion um event von Client --> Backend --> Andere Client zu senden
+            // Spiel wird angenommen bereite die Farbauswahl vor in dem das gameRequestAccepted an den Host gesendet wird
             let user = users.filter(user => user.id == socket.id)[0];
-
-            socket.broadcast.to(requestData.room).emit('gameRequestAccepted', {
+            socket.broadcast.to(user.room).emit('gameRequestAccepted', {
                 id: user.id,
                 name: user.name,
                 room: user.room,
@@ -98,14 +79,14 @@ exports = module.exports = function (io) {
             });
 
         });
-        socket.on('setOrientation', (requestData) => {
-            console.log("server seitige set Orientaion" + requestData.color)
-            let user = users.filter(user => user.id == socket.id)[0];
-            socket.broadcast.to(requestData.room).emit('setOrientationOppnt', {
-                color: requestData.color,
+        socket.on('setOrientation', (requestData) => { // middleware funktion um event von Client --> Backend --> Andere Client zu senden
+            let user = users.filter(user => user.id == socket.id)[0];     //lese den akuellen User aus um mit seinen Daten       
+            socket.broadcast.to(requestData.room).emit('setOrientationOppnt', { // dieses Event wir an die gegner Frontent Client gesendet
+                color: requestData.color,  //hier ist die entgegengesetzte Farbe zum Host drine
                 id: user.id,
                 name: user.name,
                 room: user.room,
+
             });
         });
 
@@ -113,6 +94,9 @@ exports = module.exports = function (io) {
 
 
         socket.on('gameISover', (userData) => {
+            //das Spiel ist zuende und wir haben einen gewinner.
+            // Mit dem Event getloser das in den Raum (ocket.broadcast.to(userData.room)) gesendet wird können wir den gegenspieler ausfindig machen 
+            // dieser User im Raum ist automatisch der Verlieren
             socket.broadcast.to(userData.room).emit('getloser',
                 {
                     //gehe zum Client der verloren hat und hole seine ID
@@ -121,17 +105,18 @@ exports = module.exports = function (io) {
         });
 
         socket.on('calculateELO', (userData) => {
+            //diese Funktion hat nun den gewinner und den Verlierer einer Schachpartie und kann somit die Elo bewertung in der DB durchführen
             console.log("Das Spiel ist aus. Gewonnen hat  " + userData.won)
             console.log("Das Spiel ist aus. Verloren hat  " + userData.lost)
-            db.eloupdate(userData.won, userData.lost)
+            db.eloupdate(userData.won, userData.lost) //elo in der DB ändern
         });
 
 
 
 
-        socket.on('chessMove', (requestData) => {
+        socket.on('chessMove', (requestData) => { // middleware funktion um gezogene Figure an den anderen Client zu senden.
             console.log(requestData);
-            socket.broadcast.to(requestData.room).emit('oppntChessMove', {
+            socket.broadcast.to(requestData.room).emit('oppntChessMove', { //sende den Zuginformationen an den Gegner mit dem Event  oppntChessMove
                 color: requestData.color,
                 from: requestData.from,
                 to: requestData.to,
@@ -142,13 +127,21 @@ exports = module.exports = function (io) {
         });
 
 
-      socket.on('disconnect', () => {
-          for (i = 0; i < users.length; i++) {
-              if (users[i].id == socket.id) {
-                  users.splice(i, 1);
-                  break;
-              }
-          }
-      });
+        socket.on('disconnect', () => { //Socket disconnect funktion
+            //console.log("Someone disconnected")
+            for (i = 0; i < users.length; i++) { //lösche alle infos über den User aus dem globalem user array
+                if (users[i].id == socket.id) {
+                    users.splice(i, 1);
+                    break;
+                }
+            }
+            //console.log(users)
+            socket.broadcast.emit("lobbyrefresh", { // aktualisiere die Lobby
+                users: users
+            });
+
+        });
+
+
     });// 
 }
--- src/public/js/chessgame.js
+++ src/public/js/chessgame.js
@@ -1,10 +1,9 @@
 var board = null;
 var chess = new Chess();
 var currentRoom
-var colorhost
 var gameover = 0
 var winningcolor
-var currentuser= $('#userid').html()
+var currentuser = $('#userid').html() //lese den aktuellen User aus dem html element 
 const boardConfig = {
     draggable: true,
     dropOffBoard: 'snapback',
@@ -17,20 +16,19 @@ var isMachinePlayer = false;
 board = Chessboard('chessBoard', boardConfig);
 
 function onDragStart(source, piece, position, orientation) {
-
-    if (gameover < 1) {
-        // console.log(chess.turn());
+// funktion wird aufgerufen wenn eine Figur berührt wird (angeklickt)
+    if (gameover < 1) { //überprüfe ob das Spiel vorbei ist
         if (chess.in_checkmate()) {
-            let confirm = window.confirm("You Lost! Reset the game?");
+            let confirm = window.confirm("Du befindest dich in Schachmatt und hast somit Verloren");
+            $('.notification')
+                    .html('<div class="alert alert-success">"Spielende du hast leider Verloren."</div>');
             gameover = 1
-            //let room = $('#onlinePlayers li.active button').data('room');
-            //       let room = $('#onlinePlayers li.active button').data('room');
             if (confirm) {
                 if (isMachinePlayer) {
                     chess.reset();
                     board.start();
                 } else {
-                    
+
                 }
             }
         }
@@ -48,12 +46,10 @@ function onDragStart(source, piece, position, orientation) {
 
 }
 function onDrop(source, target, piece, newPos, oldPos, orientation) {
-
-    if (gameover < 1 ) {
-        // see if the move is legal
-        let turn = chess.turn();
-        // let room = $('#onlinePlayers li.active button').data('room');
-        let move = chess.move({
+//Funktion wird ausgewählt wenn Figur letztendlich losgelassen wurd und der Zug somit bestätigt wurde
+    if (gameover < 1) {
+        let turn = chess.turn(); //überprüfe auf valieden Zug
+        let move = chess.move({ //speichere den Move in das Elemet move
             color: turn,
             from: source,
             to: target,
@@ -64,14 +60,14 @@ function onDrop(source, target, piece, newPos, oldPos, orientation) {
         // illegal move
         if (move === null) return 'snapback';
         updateStatus();
-        //player just end turn, CPU starts searching after a second
+        
         if (isMachinePlayer) {
             //window.setTimeout(chessEngine.prepareAiMove(),500);
         }
-        else {
+        else { // Zug war auf der Clientseit gültig bishier hier, und wird somit 
             socket.emit('chessMove', {
-                room: currentRoom,
-                color: turn,
+                room: currentRoom, //welcher raum ist dran
+                color: turn, //welche farbe ist am ZUg
                 from: move.from,
                 to: move.to,
                 piece: move.piece
@@ -79,7 +75,7 @@ function onDrop(source, target, piece, newPos, oldPos, orientation) {
         }
 
     } else {
-        window.alert("gameover");
+        window.alert("gameover"); //spiel ist vorbei es kommt eine meldung 
     }
 }
 
@@ -92,22 +88,16 @@ function updateStatus() {
     if (chess.in_checkmate() == true) {
         status = "You won, " + moveColor + " is in checkmate";
         gameover = 1;
-        winningcolor= (moveColor === 'black') ? 'white' : 'black'
+        winningcolor = (moveColor === 'Black') ? 'weiß' : 'schwarz'
+        $('.notification')
+        .html('<div class="alert alert-success">"Spielende --> ' + winningcolor + ' <-- hat gewonnen."</div>');
 
 
-
-        // Wenn Spiel zu ende sende dies all Room Teilnehmer 
-        console.log(currentRoom)
-        console.log("ein vor broadcast")
+        // Wenn Spiel zu ende Send das Spiel ist vorbei Event an das Backend.
         socket.emit('gameISover', {
-            userWon: currentuser,
-            room: currentRoom
+            userWon: currentuser, //Schreibe die info WER (aktueller Client) gewonnen hat
+            room: currentRoom  //welcher raum ist zu ende
         });
-
-
-
-
-        console.log(winningcolor);
         window.alert(status);
         if (isMachinePlayer) {
             chess.reset();
@@ -122,9 +112,16 @@ function updateStatus() {
     }
 }
 
+
+
+
+
+
+
 $(function () {
 
-    socket.on('oppntChessMove', (requestData) => {
+    socket.on('oppntChessMove', (requestData) => {// wenn dieses Event empfangen wird hat dein gegner ein ZUg gemacht
+        //dieses Event schreibt den Gegnerichen Zug in dein Schachfeld
         console.log("oppntChessMove")
         console.log(requestData);
         let color = requestData.color;
@@ -132,8 +129,7 @@ $(function () {
         let target = requestData.to;
         let promo = requestData.promo || '';
 
-        console.log("currentRoom in chessgame.js" + currentRoom)
-        chess.move({ from: source, to: target, promotion: promo });
+        chess.move({ from: source, to: target, promotion: promo }); // hier wird die figur bewegt 
         board.position(chess.fen());
         //chess.move(target);
         //chess.setFenPosition();
@@ -143,106 +139,42 @@ $(function () {
 
 
     $(document).on('click', '.setOrientation', function () {
-        currentRoom = $(this).data('room')
-        colorhost = $(this).data('color')
-        console.log(currentRoom)
+        currentRoom = $(this).data('room') //lese den aktuellen Raum aus dem HTML Element des Knopfes
         socket.emit('setOrientation', {
             room: $(this).data('room'), //hole room aus den Botton html tag 
+
             color: ($(this).data('color') === 'black') ? 'white' : 'black', // hier wird jenachdem welche Color gewählt wurde diese für den gegener geswitsched
-            userid: $('#userid').html(),
-            colorhost: $(this).data('color')
+            userid: $('#userid').html(), //lese die aktuelle UserID aus
         });
 
-        board.orientation($(this).data('color'));
+        board.orientation($(this).data('color'));  //starte das Schachfeld auf der Hostseite mit der ausgewählten Farbe
         board.start();
         if ($(this).data('color') == 'black') {
             $('.notification')
-                .html('<div class="alert alert-success">Great ! Let\'s start game. You choose Black. Wait for White Move.</div>');
+                .html('<div class="alert alert-success">Great ! Starten wir das Spiel. Sie wählen Schwarz. Warte auf den weißen Zug.</div>');
         } else {
             $('.notification')
-                .html('<div class="alert alert-success">Great ! Let\'s start game. You choose White. Start with First Move.</div>');
+                .html('<div class="alert alert-success">Great ! Starten wir das Spiel. Sie wählen Weiß. Beginne mit dem ersten Zug.</div>');
         }
     });
     socket.on('setOrientationOppnt', (requestData) => {
-        console.log(requestData);
+        //Dieses Event bekommt der gegner (des Host)
+        //Diesem wird gesagt wie er Sein Schachboard darzustellen hat 
+        currentRoom = requestData.room //lese den aktuellen Raum aus dem HTML Element des Knopfes
 
         board.orientation(requestData.color);
         board.start();
-        $('#onlinePlayers li#' + requestData.id).addClass('active');
+
         if (requestData.color == 'white') {
             $('.notification')
-                .html('<div class="alert alert-success">Game is initialized by <strong>' + requestData.name + '</strong>. Let\'s start with First Move.</div>');
+                .html('<div class="alert alert-success">Das Spiel wird initialisiert durch <strong>' + requestData.name + '</strong>.  Beginne mit dem ersten Zug </div>');
         } else {
             $('.notification')
-                .html('<div class="alert alert-success">Game is initialized by <strong>' + requestData.name + '</strong>. Wait for White Move.</div>');
+                .html('<div class="alert alert-success">Das Spiel wird initialisiert durch  <strong>' + requestData.name + '</strong>.  Beginne mit dem ersten Zug </div>');
         }
-
-    });
-    socket.on('startgame', (requestData) => {
-        socket.emit('setOrientation', {
-            room: $(this).data('room'),
-            color: requestData.color
-        });
-        board.orientation("black");
-        board.start();
-
-
+        //Gameinitialisierung ist hierbmit beendet
     });
 
-    socket.on('testMessage', (requestData) => {
-        console.log(requestData.id)
-        console.log("This is a test message inside chessgame.js")
-        console.log("du bist im raum")
-        alert(requestData.id)
 
 
-
-        //chess.move(target);
-        //chess.setFenPosition();
-
-    });
-    /*$(document).on('click', '.setOrientation', function(){
-        
-        socket.emit('setOrientation', {
-            room: $(this).data('room'),
-            color: ($(this).data('color') === 'black') ? 'white': 'black'
-        });
-        
-        board.orientation( $(this).data('color') );
-        board.start();
-        if($(this).data('color') == 'black'){
-            $('.notification')
-            .html('<div class="alert alert-success">Great ! Let\'s start game. You choose Black. Wait for White Move.</div>');
-        }else{
-            $('.notification')
-            .html('<div class="alert alert-success">Great ! Let\'s start game. You choose White. Start with First Move.</div>');
-        }
-    });
- 
- 
- 
-    socket.on('oppntChessMove', (requestData) => {
-        console.log(requestData);
-        let color = requestData.color;
-        let source = requestData.from;
-        let target = requestData.to;
-        let promo = requestData.promo||'';
- 
- 
-        chess.move({from:source,to:target,promotion:promo});
-        board.position(chess.fen());
-        //chess.move(target);
-        //chess.setFenPosition();
- 
-    });
- 
-    socket.on('oppntWon', (requestData) => {
-        $('.notification')
-        .html('<div class="alert alert-success">You Won !!</div>');
-        chess.reset();
-        board.reset();
-    });
-    */
 });
-
-
